a100065121dcd076b430dc5cb0533efb8808f5e2
Rafael <rcarlosama@misena.edu.co>
2018-05-03 19:07:38 -0500

    Subiendo archivos

---
+++ calculadora.js
@@ -0,0 +1,33 @@
+function fsuma(){
+ n1=document.f1.num1.value;
+ n2=document.f1.num2.value;
+res=document.f1.resul.value;
+res=parseInt(n1)+parseInt(n2);
+document.f1.resul.value=res;
+ 
+}
+function fresta(){
+ n1=document.f1.num1.value;
+ n2=document.f1.num2.value;
+res=document.f1.resul.value;
+res=parseInt(n1)-parseInt(n2);
+document.f1.resul.value=res;
+ 
+}
+function fmultiplicacion(){
+ n1=document.f1.num1.value;
+ n2=document.f1.num2.value;
+res=document.f1.resul.value;
+res=parseInt(n1)*parseInt(n2);
+document.f1.resul.value=res;
+ 
+}
+function fdivicion(){
+ n1=document.f1.num1.value;
+ n2=document.f1.num2.value;
+res=document.f1.resul.value;
+res=parseInt(n1)/parseInt(n2);
+document.f1.resul.value=res;
+}
+ 
+ 
